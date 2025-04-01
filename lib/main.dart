a1ea47f8ff1d0a9b411f61aaf44f27337154907b
Majok-Bol <bolmajok001@gmail.com>
2025-04-01 22:25:19 +0300

    layout builder widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,16 @@ void main(){
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
+    const items=5;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: Text('List items with space',style: TextStyle(color: Colors.white,fontSize: 25),),centerTitle: true,backgroundColor: Colors.purple,),
+        body: LayoutBuilder(builder:(context,constraints){
+          return SingleChildScrollView(
+            child: ConstrainedBox(constraints:BoxConstraints(minHeight: constraints.maxHeight)),
+          );
+        }),
       ),
     );
   }
