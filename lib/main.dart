e7c866e9aea7ca6ca3493b5dbf4fb4b6aa10b78a
Majok-Bol <bolmajok001@gmail.com>
2025-04-01 22:40:49 +0300

    added card widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,24 @@ class MyApp extends StatelessWidget{
         appBar: AppBar(title: Text('List items with space',style: TextStyle(color: Colors.white,fontSize: 25),),centerTitle: true,backgroundColor: Colors.purple,),
         body: LayoutBuilder(builder:(context,constraints){
           return SingleChildScrollView(
-            child: ConstrainedBox(constraints:BoxConstraints(minHeight: constraints.maxHeight)),
+            child: ConstrainedBox(constraints:BoxConstraints(minHeight: constraints.maxHeight),child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+
+
+              children:List.generate(items, (index)=>ItemWidget(text: 'Text $index')),
+            ),),
           );
         }),
       ),
     );
+  }
+}
+class ItemWidget extends StatelessWidget{
+  const ItemWidget({super.key,required this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context){
+    return Card(child: SizedBox(height:100,child:Center(child:  Text(text),),),);
   }
 }
